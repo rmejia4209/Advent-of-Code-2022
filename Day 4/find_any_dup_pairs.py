0653b79878c1b6fc200e43dc457d49c45f2851ef
Rigo Mejia <rmejia4209@gmail.com>
2022-12-18 19:51:53 -0600

    Adding Day 4

---
+++ Day 4/find_any_dup_pairs.py
@@ -0,0 +1,19 @@
+# Author: Rigoberto Mejia
+# GitHub username: rmejia4209
+# Date: 12/06/2022
+
+num_matches = 0
+
+with open("pairs.txt", "r") as pairs:
+    for pair in pairs:
+        pair = pair.replace("\n", "")
+        pair = pair.split(",")
+        pair_one = list(range(int(pair[0].split("-")[0]), int(pair[0].split("-")[1])+1))
+        pair_two = list(range(int(pair[1].split("-")[0]), int(pair[1].split("-")[1])+1))
+
+        if any(item in pair_one for item in pair_two) or any(item in pair_two for item in pair_one):
+            print(pair_one)
+            print(pair_two)
+            num_matches += 1
+
+print("Matches:", num_matches)
